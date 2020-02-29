13127d27400074c1c95481da97451e1198e6352a
zhangxu147 <zhangxu595@126.com>
2020-02-29 16:35:04 +0800

    提交项目

---
+++ ZXBlog.Model/role.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZXBlog.Model
+{
+    /// <summary>
+    /// 角色表
+    /// </summary>
+    public class role:BaseEntity
+    {
+        /// <summary>
+        /// 角色名称
+        /// </summary>
+        [StringLength(maximumLength: 100),Required,Column(TypeName = "varchar")]
+        public string role_name { get; set; }
+    }
+}
