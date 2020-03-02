08dd692e49fc617714dadafc03c57189ffe99a46
zhangxu147 <zhangxu595@126.com>
2020-03-02 15:56:35 +0800

    Merge branch 'master' of https://github.com/zhangxu0910/ZXBlog
    # Conflicts:
    #	ZXBlog/ZXBlog.WebApi/ZXBlog.WebApi.csproj

--- ZXBlog/ZXBlog.WebApi/Global.asax.cs
+++ ZXBlog/ZXBlog.WebApi/Global.asax.cs
@@ -1,9 +1,6 @@
-using Autofac;
-using Autofac.Integration.WebApi;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
@@ -12,37 +9,8 @@ namespace ZXBlog.WebApi
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
-        private void InAutofac()
-        {
-            var configuration = GlobalConfiguration.Configuration;
-            var builder = new ContainerBuilder();
-            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
-            builder.RegisterWebApiFilterProvider(configuration);
-
-
-            Assembly[] services =
-        {
-            Assembly.Load("ZXBlog.IDAL"),
-            Assembly.Load("ZXBlog.DAL"),
-            Assembly.Load("ZXBlog.IBLL"),
-            Assembly.Load("ZXBlog.BLL")
-        };
-
-            builder.RegisterAssemblyTypes(services).Where(r => !r.IsAbstract).AsImplementedInterfaces().SingleInstance()
-                .PropertiesAutowired();
-
-
-            var container = builder.Build();
-
-            var resolver = new AutofacWebApiDependencyResolver(container);
-            configuration.DependencyResolver = resolver;
-        }
-
-
         protected void Application_Start()
         {
-            InAutofac();
-
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
